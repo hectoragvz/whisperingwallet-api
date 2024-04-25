466cd782960a7d97711d8904de349b4dd2f222de
hectoragvz <hectoragvz@gmail.com>
2024-04-25 09:16:46 -0700

    total field mod

--- api/models.py
+++ api/models.py
@@ -8,7 +8,7 @@ class Expense(models.Model):
     url = models.TextField(max_length=255)
     # response = models.JSONField()
     product = models.CharField(max_length=100)
-    total = models.DecimalField(max_digits=5, decimal_places=2)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="expense")
